c0e988deaf5709d109f646e3f233a8190c3d5600
Alvaro F Boirac <alphydan@gmail.com>
2016-12-31 17:25:16 -0400

    new sketches

--- neuron1/sketch.js
+++ neuron1/sketch.js
@@ -1,71 +1,30 @@
-var Axon0 = {
-    X : 320, Y : 460,
-    sizeX : 30, sizeY : 30,
-    R : 200, G : 200, B : 200,
-};
-
-var AxonL = {
-    X : 310, Y : 460,
-    sizeX : 30, sizeY : 30,
-    R : 200, G : 200, B : 200,
-};
-
-var AxonR = {
-    X : 320, Y : 462,
-    sizeX : 30, sizeY : 30,
-    R : 200, G : 200, B : 200,
-};
-
-var AxonBR = {
-    X : 340, Y: 500,
-    sizeX : 0, sizeY : 0,
-     }
-
-
-
 function setup() {
-  createCanvas(640, 480);
-  background(0)
+    createCanvas(600, 1000);
+    rectMode(CENTER);
 }
 
 function draw(){
-    noStroke()
+    background(240);
 
+translate(width/2, height/2);
 
+push();
+translate(50, 50);
+rotate(PI/3);
+quad(-25, -25, -25, 25, 25, 25, 25, -25);
+pop();
 
-    // Vertical Axon0
-    ellipse(Axon0.X, Axon0.Y, Axon0.sizeX, Axon0.sizeY)
-    Axon0.X = Axon0.X - 1 + random(2)
-    Axon0.Y = Axon0.Y - 2
-    Axon0.sizeX = Axon0.sizeX/(1+0.8*random(0.1))+random(0.2)
-    Axon0.sizeY = Axon0.sizeY/(1+0.8*random(0.1))+random(0.2)
+push();
+translate(50, 50);
+rotate(2*PI/3);
+quad(-25, -25, -25, 25, 25, 25, 25, -25);
+pop();
 
-
-    // Left AxonL
-    ellipse(AxonL.X, AxonL.Y, AxonL.sizeX, AxonL.sizeY)
-    AxonL.X = AxonL.X - 2 + random(2)
-    AxonL.Y = AxonL.Y - 1+2.1*random(-1,1)
-    AxonL.sizeX = AxonL.sizeX/(1+0.5*random(0.1))+random(0.2)
-    AxonL.sizeY = AxonL.sizeY/(1+0.5*random(0.1))+random(0.2)
-
-    // Right AxonR
-    ellipse(AxonR.X, AxonR.Y, AxonR.sizeX, AxonR.sizeY)
-    AxonR.X = AxonR.X + 2 + random(2)
-    AxonR.Y = AxonR.Y - 1+1*random([0,0,0,0,2,-2])
-    AxonR.sizeX = AxonR.sizeX/(1+0.5*random(0.1))+random(0.2)
-    AxonR.sizeY = AxonR.sizeY/(1+0.5*random(0.1))+random(0.2)
-
-    // First AxonR Branch
-    // ellipse(AxonBR.X, AxonBR.Y, AxonBR.sizeX, AxonBR.sizeY)
-    // if (AxonR.X = 420){
-    //     AxonBR.X = AxonR.X;
-    //     AxonBR.Y = AxonR.Y;
-    //     AxonBR.sizeX = AxonR.sizeX;
-    //     AxonBR.sizeY = AxonR.sizeY;
-    // }
-
+push();
+translate(50, 50);
+rotate(-3*PI/3);
+quad(-25, -25, -25, 25, 25, 25, 25, -25);
+pop();
 
 
 }
-
-
